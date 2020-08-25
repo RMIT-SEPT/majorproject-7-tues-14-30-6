6e0f6f98753847b4d5bf9effda89201951ce79bb
kayeval <kaye_ng@dlsu.edu.ph>
2020-08-25 15:11:13 +1000

    add getter and setter for username field

--- BackEnd/apppointment-service-backend/src/main/java/com/rmit/sept/tues06/apppointmentservicebackend/model/User.java
+++ BackEnd/apppointment-service-backend/src/main/java/com/rmit/sept/tues06/apppointmentservicebackend/model/User.java
@@ -39,6 +39,14 @@ public abstract class User {
         this.name = name;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getAddress() {
         return address;
     }
