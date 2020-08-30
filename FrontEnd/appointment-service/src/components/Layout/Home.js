9071cbc3500320a430aa13e86939da653546e108
s3783156 <s3783156@student.rmit.edu.au>
2020-08-30 17:57:25 +1000

    Two rows of home page implmented. May implmenet more later

--- FrontEnd/appointment-service/src/components/Layout/Home.js
+++ FrontEnd/appointment-service/src/components/Layout/Home.js
@@ -3,6 +3,7 @@ import doctor from "../images/doctors.png";
 import './Home.css'
 import Card from 'react-bootstrap/Card'
 import Image from 'react-bootstrap/Image'
+import laptop from "../images/laptop_booking.jpg";
 
 export default class Home extends Component {
     render() {
@@ -17,42 +18,42 @@ export default class Home extends Component {
                 {/* first inline block */}
                 <div className="block-content clearfix">
 
-                        <div className="card text-black d-inline-block card-left" style={{ width: '22rem' }}>
-                            <Card.Body>
-                                <Card.Title><h3>Who we are</h3></Card.Title>
-                                <Card.Text>
-                                   <p>Here at AGME, we provide booking services for bussinesses to integrate into their site.
+                    <Card className="bg-primary text-white d-inline-block card-left" style={{ width: '22rem' }}>
+                        <Card.Body>
+                            <Card.Title><h3>Who we are</h3></Card.Title>
+                            <Card.Text>
+                                <p>Here at AGME, we provide booking services for bussinesses to integrate into their site.
                                        <br></br><br></br>
                                        Through us, you will be able to provide working booking services for your customers.
                                    </p>
-                                </Card.Text>
-                            </Card.Body>
-                        </div>
-                    <Image src={doctor} className="d-inline-block float-right" id="doctor" alt="Doctors appointment" rounded />
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Image src={doctor} className="d-inline-block float-right image"  alt="Doctors appointment" rounded />
 
                 </div>
                 {/* end of first inline block */}
 
                 {/* second inline block */}
                 <div className="block-content clearfix">
-                <Image src={doctor} className="d-inline-block float-left" id="doctor" alt="Doctors appointment" rounded />
+                    <Image src={laptop} className="d-inline-block float-left image"  alt="Laptop booking" rounded />
 
-                <div className="card text-black d-inline-block card-right" style={{ width: '22rem' }}>
-                            <Card.Body>
-                                <Card.Title><h3>Why choose us</h3></Card.Title>
-                                <Card.Text>
-                                   <p>
-                                    Not only are we one of the cheapest services out there, we also make our booking service 
-                                    easy to integrate into your site. 
+                    <Card className="bg-primary text-white d-inline-block card-right" style={{ width: '22rem' }}>
+                        <Card.Body>
+                            <Card.Title><h3>Why choose us</h3></Card.Title>
+                            <Card.Text>
+                                <p>
+                                    Not only are we one of the cheapest services out there, we also make our booking service
+                                    easy to integrate into your site.
                                     <br></br>
-                                    So you can get your booking service up and running quick and easily.
+                                    So you can get your booking service up and running quickly and easily.
                                    </p>
-                                </Card.Text>
-                            </Card.Body>
-                        </div>
-                    
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+
                 </div>
-{               /*end of second inline block */} 
+                {               /*end of second inline block */}
                 {/* end of content */}
             </div >
         )
