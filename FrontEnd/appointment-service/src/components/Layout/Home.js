1bf52858f8d9a7d8700d9acac4980bcf65e24d75
s3783156 <s3783156@student.rmit.edu.au>
2020-08-30 11:37:22 +1000

    Implmenting home page with cards and images

--- FrontEnd/appointment-service/src/components/Layout/Home.js
+++ FrontEnd/appointment-service/src/components/Layout/Home.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react'
 import doctor from "../images/doctors.png";
 import './Home.css'
+import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
 
 export default class Home extends Component {
     render() {
         return (
-            <div id = "homePage">
+            <div id="homePage">
+                {/*Title*/}
                 <h1 className="display-5 text-center">Welcome to AGME</h1>
                 <h5 className="display-5 text-center">Essential booking service for any bussiness</h5>
-                {/* This is from the boot strap classes. 
-                <p className="alert alert-warning"> Warning</p>
-                */}
-                <img src={doctor} id = "doctor" alt="Doctors appointment" />
-            </div>
+                {/* end of title */}
+
+                {/* content */}
+                {/* Perhaps display inline block? */}
+                <div className="block-content">
+                    <div className="card bg-primary text-white" style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Who we are</Card.Title>
+                            <Card.Text>
+                                We are a service for busines
+                     </Card.Text>
+                        </Card.Body>
+                    </div>
+
+                    <div className="align-right">
+                        <Image src={doctor}  id="doctor" alt="Doctors appointment" rounded />
+                    </div>
+                </div>
+                {/* end of content */}
+
+            </div >
         )
     }
 }
