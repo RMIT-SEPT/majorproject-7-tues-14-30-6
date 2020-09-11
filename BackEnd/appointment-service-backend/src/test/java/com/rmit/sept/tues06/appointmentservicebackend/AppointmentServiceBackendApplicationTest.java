68851fa3d63ec726960f05ee12f401627d513292
ibrokemypie <ibrokemypie@bastardi.net>
2020-09-11 14:59:19 +1000

    Add application test

---
+++ BackEnd/appointment-service-backend/src/test/java/com/rmit/sept/tues06/appointmentservicebackend/AppointmentServiceBackendApplicationTest.java
@@ -0,0 +1,17 @@
+package com.rmit.sept.tues06.appointmentservicebackend;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@SpringBootTest
+public class AppointmentServiceBackendApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
