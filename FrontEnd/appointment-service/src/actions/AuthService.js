cc3bc048045b760bd527cbd57ff3596a742a570f
s3783156 <s3783156@student.rmit.edu.au>
2020-09-13 20:41:12 +1000

    Implemented sign up and log in. Notifies user on success or failure

--- FrontEnd/appointment-service/src/actions/AuthService.js
+++ FrontEnd/appointment-service/src/actions/AuthService.js
@@ -2,17 +2,38 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/auth/";
 
-// used for registering an account
-const register = (newAccount, message) => {
+// the changes made to the status display are in both clauses.
+// faster response times is why.
+
+// register an account
+const register = (newAccount) => {
+  document.getElementById("status").style.display = "block";
   axios.post(API_URL + "register", JSON.parse(JSON.stringify(newAccount))).then((response) => {
-    
     document.getElementById("message").innerHTML = response.data.message;
+    document.getElementById("status").style.display = "block";
   }, (error) => {
+    document.getElementById("status").style.display = "block";
     document.getElementById("message").innerHTML = "Error! Failed to sign up!";
   });
-  // display the message
-  document.getElementById("status").style.display = "block";
+
 }
 
+// login
+const login = (account) => {
+
+  axios.post(API_URL + "login", JSON.parse(JSON.stringify(account))).then((response) => {
+    document.getElementById("status").style.display = "block";
+    document.getElementById("message").innerHTML = "Successfully logged in as" + response.data.username;
+  }, (error) => {
+    document.getElementById("message").innerHTML = "Failed to log in!";
+    document.getElementById("status").style.display = "block";
+  });
+
+}
+
+
 // put the other const in here as an array
-export default { register };
+export default {
+  register,
+  login
+};
