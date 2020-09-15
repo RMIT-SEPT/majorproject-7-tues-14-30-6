afb9786737ee1a6aeedca02e497a52629955a709
ibrokemypie <ibrokemypie@bastardi.net>
2020-09-15 15:19:13 +1000

    Add test for user password too long

--- BackEnd/appointment-service-backend/src/test/java/com/rmit/sept/tues06/appointmentservicebackend/UserServiceTest.java
+++ BackEnd/appointment-service-backend/src/test/java/com/rmit/sept/tues06/appointmentservicebackend/UserServiceTest.java
@@ -7,11 +7,13 @@ import com.rmit.sept.tues06.appointmentservicebackend.service.UserService;
 import com.rmit.sept.tues06.appointmentservicebackend.service.UserServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceTest {
+    @Autowired
     private final UserService userService = new UserServiceImpl();
 
     @Test
@@ -40,5 +42,11 @@ public class UserServiceTest {
         User customer = new Customer("test3","test@test.com","123456","test","test test","1");
         User user = userService.createUser(customer);
         Assertions.assertNotNull(user);
+    }
+
+    @Test void When_LongPassword_Expect_UserException() {
+        String password = new String(new char[121]).replace('\0', 'a');
+        User user = new Customer("test4","test@test.com",password,"test","test test","1");
+        Assertions.assertThrows(UserException.class,()->userService.createUser(user));
     }
 }
