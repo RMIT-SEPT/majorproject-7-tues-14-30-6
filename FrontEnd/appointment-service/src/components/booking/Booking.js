3d2f5b91234ffd9b414eb9f28dfbbe6ad19b0307
s3783156 <s3783156@student.rmit.edu.au>
2020-09-18 20:08:53 +1000

    Implementing front end page. Currently gets data for future bookings

--- FrontEnd/appointment-service/src/components/booking/Booking.js
+++ FrontEnd/appointment-service/src/components/booking/Booking.js
@@ -3,27 +3,31 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './Booking.css'
 import axios from "axios";
 
+const API_BOOKING_URL = "http://localhost:8080/api/bookings?past=false&current=true&customer=";
+
 export default class Booking extends Component {
     constructor() {
         super();
 
         this.state = {
-            users: []
+            bookings: []
         };
     }
 
-    // get the users before the page is loaded
     componentDidMount() {
-        axios.get('http://localhost:8080/api/users')
+        let username = JSON.parse(sessionStorage.getItem("user")).username;
+        axios.get(API_BOOKING_URL + username)
             .then(response => response.data)
             .then(data => {
-                this.setState({ users: data });
+                this.setState({ bookings: data });
+            }, (error) => {
             });
+
     }
 
 
     render() {
-
+        console.log(this.state.bookings);
         // display message if user is not logged in
         if (!sessionStorage.getItem("user")) {
             return (
@@ -36,13 +40,28 @@ export default class Booking extends Component {
         // else display the standard page
         return (
             <div id="main">
-                <div id="viewBookings">
+                <div id="addBooking">
                     <h5 className="display-5 text-center">Add a booking*</h5>
                     <hr style={{ margin: "3px" }}></hr>
                 </div>
                 <h2 className="display-5 text-center">Bookings</h2>
                 <h6 className="display-5 text-center">On this page, you can view all
                 your upcoming bookings and also create new bookings</h6>
+
+                <div id="viewBookings">
+                    <h4>Your upcoming bookings</h4>
+                    <div id="bookingCardArea">
+                        {this.state.bookings.length === 0 ?
+                            <h4>No upcoming bookings</h4>
+                            :
+                            <div id="bookingCard">
+                                Some bookings
+                                </div>
+                        }
+                        {/* loop here to generate booking cards */}
+                    </div>
+                </div>
+
                 <p id="bottomText">* We only offer services for <b>Dan's Dentist</b> and <b>Bob's Bowling Ally</b> at this moment</p>
             </div>
         )
