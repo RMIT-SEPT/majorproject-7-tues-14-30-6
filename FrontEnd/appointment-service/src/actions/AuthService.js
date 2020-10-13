255bd487ce9dcbc919c086f2f1ed8fd8171fcb0d
ibrokemypie <ibrokemypie@bastardi.net>
2020-10-13 16:10:40 +1100

    Update the hardcoded paths to default to port 8080 Compile the react frontend with REACT_APP_PROD=true otherwise

--- FrontEnd/appointment-service/src/actions/AuthService.js
+++ FrontEnd/appointment-service/src/actions/AuthService.js
@@ -1,28 +1,39 @@
 import axios from "axios";
 
-const API_URL = "/api/auth/";
+let BASE_URL = "";
+if (process.env.REACT_APP_PROD !== "true") {
+  BASE_URL = "http://localhost:8080";
+}
+
+const API_URL = BASE_URL + "/api/auth/";
 
 // register an account
 const register = (newAccount) => {
-  axios.post(API_URL + "register", JSON.parse(JSON.stringify(newAccount))).then((response) => {
-    window.location.href = '/login';
-  }, (error) => {
-    document.getElementById("status").style.display = "block";
-    document.getElementById("message").innerHTML = "Error! Failed to sign up!";
-  });
-}
+  axios.post(API_URL + "register", JSON.parse(JSON.stringify(newAccount))).then(
+    (response) => {
+      window.location.href = "/login";
+    },
+    (error) => {
+      document.getElementById("status").style.display = "block";
+      document.getElementById("message").innerHTML =
+        "Error! Failed to sign up!";
+    }
+  );
+};
 
 // login
 const login = (account) => {
-  axios.post(API_URL + "login", JSON.parse(JSON.stringify(account))).then((response) => {
-    sessionStorage.setItem('user', JSON.stringify(response.data));
-    window.location.href = '/ ';
-  }, (error) => {
-    document.getElementById("message").innerHTML = "Failed to log in!";
-    document.getElementById("status").style.display = "block";
-  });
-}
-
+  axios.post(API_URL + "login", JSON.parse(JSON.stringify(account))).then(
+    (response) => {
+      sessionStorage.setItem("user", JSON.stringify(response.data));
+      window.location.href = "/ ";
+    },
+    (error) => {
+      document.getElementById("message").innerHTML = "Failed to log in!";
+      document.getElementById("status").style.display = "block";
+    }
+  );
+};
 
 // put the other const in here as an array
 export default {
--- FrontEnd/appointment-service/src/actions/BookingService.js
+++ FrontEnd/appointment-service/src/actions/BookingService.js
@@ -1,26 +1,34 @@
 import axios from "axios";
 
-const BOOKING_URL = "/api/bookings/add";
-
-const book = (newBooking) => {
-    let user = JSON.parse(sessionStorage.getItem("user"));
-    let tokenType = user.tokenType;
-    let accessToken = user.accessToken;
-    let authorization = tokenType + " " + accessToken;
-
-    axios.post(BOOKING_URL, JSON.parse(JSON.stringify(newBooking)),
-        {
-            headers: {
-                "Authorization": authorization
-            }
-        }
-    ).then((response) => {
-        window.location.href = '/booking';
-    }, (error) => {
-        // display error message that booking failed
-    });
+let BASE_URL = "";
+if (process.env.REACT_APP_PROD !== "true") {
+  BASE_URL = "http://localhost:8080";
 }
 
+const BOOKING_URL = BASE_URL + "/api/bookings/add";
+
+const book = (newBooking) => {
+  let user = JSON.parse(sessionStorage.getItem("user"));
+  let tokenType = user.tokenType;
+  let accessToken = user.accessToken;
+  let authorization = tokenType + " " + accessToken;
+
+  axios
+    .post(BOOKING_URL, JSON.parse(JSON.stringify(newBooking)), {
+      headers: {
+        Authorization: authorization,
+      },
+    })
+    .then(
+      (response) => {
+        window.location.href = "/booking";
+      },
+      (error) => {
+        // display error message that booking failed
+      }
+    );
+};
+
 export default {
-    book
+  book,
 };
