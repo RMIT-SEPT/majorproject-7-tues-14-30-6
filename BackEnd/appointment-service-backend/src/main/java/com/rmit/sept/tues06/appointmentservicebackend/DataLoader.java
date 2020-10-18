d36d1768f12471e34a87b2566646f22829ad7f0a
kayeval <kaye_ng@dlsu.edu.ph>
2020-10-18 15:46:31 +1100

    encode password and set roles when prepopulating db with test users

--- BackEnd/appointment-service-backend/src/main/java/com/rmit/sept/tues06/appointmentservicebackend/DataLoader.java
+++ BackEnd/appointment-service-backend/src/main/java/com/rmit/sept/tues06/appointmentservicebackend/DataLoader.java
@@ -1,41 +1,85 @@
 package com.rmit.sept.tues06.appointmentservicebackend;
 
-import com.rmit.sept.tues06.appointmentservicebackend.model.Admin;
-import com.rmit.sept.tues06.appointmentservicebackend.model.Customer;
-import com.rmit.sept.tues06.appointmentservicebackend.model.User;
-import com.rmit.sept.tues06.appointmentservicebackend.model.Worker;
+import com.rmit.sept.tues06.appointmentservicebackend.model.*;
+import com.rmit.sept.tues06.appointmentservicebackend.repository.RoleRepository;
 import com.rmit.sept.tues06.appointmentservicebackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Component
-public class DataLoader implements ApplicationRunner {
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
 
-    private final UserService userService;
-    private final PasswordEncoder encoder;
+@Component
+public class DataLoader {
+    private static int counter;
 
     @Autowired
-    public DataLoader(UserService userService, PasswordEncoder encoder) {
-        this.userService = userService;
-        this.encoder = encoder;
-    }
+    private UserService userService;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private PasswordEncoder encoder;
 
-    public void run(ApplicationArguments args) {
-        if (userService.findAllUsers().spliterator().getExactSizeIfKnown() == 0) {
+    @PostConstruct
+    public void init() {
+        // initialize your monitor here, instance of someService is already injected by this time.
+        counter = 0;
+
+        while (counter < 5) {
+            Set<Role> roles = new HashSet<>();
+
+            Role customerRole1 = new Role();
+            Role customerRole2 = new Role();
+            customerRole1.setName(ERole.ROLE_CUSTOMER);
+            customerRole2.setName(ERole.ROLE_CUSTOMER);
+
+            Role workerRole1 = new Role();
+            Role workerRole2 = new Role();
+            workerRole1.setName(ERole.ROLE_WORKER);
+            workerRole2.setName(ERole.ROLE_WORKER);
+
+            Role adminRole = new Role();
+            adminRole.setName(ERole.ROLE_ADMIN);
+
+            roles.add(customerRole1);
             User customer1 = new Customer("customer", "customer1@email.com", encoder.encode("password"), "Jane D.", "123 Swanston Street", "0444984847");
-            User customer2 = new Customer("customer2", "customer2@email.com", encoder.encode("password"), "John S.", "456 Swanston Street", "0422324847");
-            User worker1 = new Worker("worker", "worker@email.com", encoder.encode("password"), "James R.", "9 Bourke Street", "0422368916");
-            User worker2 = new Worker("worker2", "worker2@email.com", encoder.encode("password"), "Eliza M.", "4 Akuna Avenue", "041212121");
-            User admin = new Admin("admin", "admin@email.com", encoder.encode("password"), "Valerie N.", "9 Spencer Street", "041212121");
+            customer1.setRoles(roles);
 
-            userService.createUser(customer1);
-            userService.createUser(customer2);
-            userService.createUser(worker1);
-            userService.createUser(worker2);
-            userService.createUser(admin);
+            roles.clear();
+            roles.add(customerRole2);
+            User customer2 = new Customer("customer2", "customer2@email.com", encoder.encode("password"), "John S.", "456 Swanston Street", "0422324847");
+            customer2.setRoles(roles);
+
+            roles.clear();
+            roles.add(workerRole1);
+            User worker1 = new Worker("worker", "worker@email.com", encoder.encode("password"), "James R.", "9 Bourke Street", "0422368916");
+            worker1.setRoles(roles);
+
+            roles.clear();
+            roles.add(workerRole2);
+            User worker2 = new Worker("worker2", "worker2@email.com", encoder.encode("password"), "Eliza M.", "4 Akuna Avenue", "041212121");
+            worker2.setRoles(roles);
+
+            roles.clear();
+            roles.add(adminRole);
+            User admin = new Admin("admin", "admin@email.com", encoder.encode("password"), "Valerie N.", "9 Spencer Street", "041212121");
+            admin.setRoles(roles);
+
+            if (counter == 0) {
+                userService.createUser(customer1);
+            } else if (counter == 1) {
+                userService.createUser(customer2);
+            } else if (counter == 2) {
+                userService.createUser(worker1);
+            } else if (counter == 3) {
+                userService.createUser(worker2);
+            } else {
+                userService.createUser(admin);
+            }
+
+            ++counter;
         }
     }
 }
